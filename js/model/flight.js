627ca26014244369db85e9b8c1181f583cf6d48b
repkit <ionut.baches@yahoo.com>
2015-05-21 17:21:28 +0000

    created flight model

---
+++ js/model/flight.js
@@ -0,0 +1,19 @@
+// js/model/flight.js
+var app = app || {};
+
+// Flight Model
+// ------------
+// Our flight model
+
+app.Flight = Backbone.Model.extend({
+  defaults: {
+    'ItineraryCode': null,
+    'Price': null,
+    'Currency': null,
+    'Routes': [],
+    'DepartureRoute': {},
+    'ReturnRoute': {},
+  },
+  
+});
+
